1e3ce82e7ee52095862cf4c9f427f0bc6e6595aa
Youssef Aly <youssefaly43@gmail.com>
2018-03-14 19:59:30 +0100

    Fix

--- frontend/components/HelloWorld.js
+++ frontend/components/HelloWorld.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const HelloWorld = () => {
+  console.log("this is only a test component");
   return (
     <div className='hello-world'>
       <h1>Hello World</h1>
       <p>Welcome to my world</p>
     </div>
   )
-}dffsdfsdf
+}
 
 export default HelloWorld
